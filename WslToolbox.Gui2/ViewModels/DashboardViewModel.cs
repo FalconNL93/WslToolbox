2fa1d679538479218fa1f86b1d3e65628167bd0e
Peter van der Veen <p.vanderveen93@gmail.com>
2022-08-03 14:54:44 +0200

    Move logic to VM

--- WslToolbox.Gui2/ViewModels/DashboardViewModel.cs
+++ WslToolbox.Gui2/ViewModels/DashboardViewModel.cs
@@ -4,8 +4,12 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
+using Wpf.Ui.Common;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Controls.Interfaces;
 using Wpf.Ui.Mvvm.Contracts;
+using WslToolbox.Gui2.Forms;
 using WslToolbox.Gui2.Models;
 using WslToolbox.Gui2.Services;
 
@@ -13,6 +17,7 @@ namespace WslToolbox.Gui2.ViewModels;
 
 public class DashboardViewModel : ObservableObject, INavigationAware
 {
+    private readonly IDialogControl _dialogControl;
     private readonly ILogger<DashboardViewModel> _logger;
     private readonly DistributionService _service;
     private readonly ISnackbarService _snackbarService;
@@ -20,26 +25,28 @@ public class DashboardViewModel : ObservableObject, INavigationAware
     public DashboardViewModel(
         ILogger<DashboardViewModel> logger,
         DistributionService service,
-        ISnackbarService snackbarService
+        ISnackbarService snackbarService,
+        IDialogService dialogService
     )
     {
         _logger = logger;
         _service = service;
         _snackbarService = snackbarService;
+        _dialogControl = dialogService.GetDialogControl();
 
         RefreshDistributions = new AsyncRelayCommand(OnRefreshDistributions);
         StartDistribution = new AsyncRelayCommand<DistributionModel>(OnStartDistribution, model => model?.State != DistributionService.StateRunning);
         StopDistribution = new AsyncRelayCommand<DistributionModel>(OnStopDistribution, model => model?.State == DistributionService.StateRunning);
-        DeleteDistribution = new AsyncRelayCommand<DistributionModel>(OnDeleteDistribution);
+        DeleteDistribution = new RelayCommand<DistributionModel>(OnDeleteDistribution);
         ExportDistribution = new AsyncRelayCommand<DistributionModel>(OnExportDistribution);
-        EditDistribution = new RelayCommand<UpdateModel<DistributionModel>>(OnEditDistribution);
+        EditDistribution = new AsyncRelayCommand<DistributionModel>(OnEditDistribution);
     }
 
     public AsyncRelayCommand RefreshDistributions { get; }
     public AsyncRelayCommand<DistributionModel> StartDistribution { get; }
     public AsyncRelayCommand<DistributionModel> StopDistribution { get; }
-    public AsyncRelayCommand<DistributionModel> DeleteDistribution { get; }
-    public RelayCommand<UpdateModel<DistributionModel>> EditDistribution { get; }
+    public RelayCommand<DistributionModel> DeleteDistribution { get; }
+    public AsyncRelayCommand<DistributionModel> EditDistribution { get; }
     public AsyncRelayCommand<DistributionModel> ExportDistribution { get; }
     public ObservableCollection<DistributionModel> Distributions { get; } = new();
 
@@ -52,56 +59,66 @@ public class DashboardViewModel : ObservableObject, INavigationAware
     {
     }
 
-    private void OnEditDistribution(UpdateModel<DistributionModel>? distribution)
+    private async Task OnStartDistribution(DistributionModel distribution)
     {
-        if (distribution == null)
-        {
-            return;
-        }
-
-        _service.RenameDistributions(distribution);
-    }
-
-    private async Task OnDeleteDistribution(DistributionModel? distribution)
-    {
-        if (distribution == null)
-        {
-            return;
-        }
-
-        await _service.DeleteDistribution(distribution);
-    }
-
-    private async Task OnStartDistribution(DistributionModel? distribution)
-    {
-        if (distribution == null)
-        {
-            return;
-        }
-
         await _service.StartDistribution(distribution);
     }
 
-    private async Task OnStopDistribution(DistributionModel? distribution)
+    private async Task OnStopDistribution(DistributionModel distribution)
     {
-        if (distribution == null)
-        {
-            return;
-        }
-
         await _service.StopDistribution(distribution);
     }
 
-    private async Task OnExportDistribution(DistributionModel? distribution)
+    private async Task OnExportDistribution(DistributionModel distribution)
+    {
+        await _service.ExportDistribution(distribution);
+    }
+
+    private async Task OnEditDistribution(DistributionModel? distribution)
     {
         if (distribution == null)
         {
             return;
         }
 
-        _snackbarService.Show("Exporting", "Distribution is exporting");
-        await _service.ExportDistribution(distribution);
-        _snackbarService.Show("Exporting", "Done!");
+        var model = new UpdateModel<DistributionModel>
+        {
+            CurrentModel = distribution,
+            NewModel = distribution.Clone()
+        };
+
+        _dialogControl.Content = new EditDistributionForm {Distribution = model.NewModel};
+        _dialogControl.ButtonLeftName = "Save";
+        _dialogControl.ButtonRightName = "Cancel";
+        var dialogResult = await _dialogControl.ShowAndWaitAsync($"Rename - {model.NewModel.Name}", "Enter a new name for your distribution");
+
+        switch (dialogResult)
+        {
+            case IDialogControl.ButtonPressed.Left:
+                _service.RenameDistributions(model);
+                _dialogControl.Hide();
+                break;
+            case IDialogControl.ButtonPressed.Right:
+            case IDialogControl.ButtonPressed.None:
+            default:
+                _dialogControl.Hide();
+                break;
+        }
+    }
+
+    private void OnDeleteDistribution(DistributionModel distribution)
+    {
+        var messageBox = new MessageBox
+        {
+            ButtonLeftName = "Delete",
+            ButtonRightName = "Cancel",
+            ButtonLeftAppearance = ControlAppearance.Danger
+        };
+
+        messageBox.ButtonLeftClick += async (_, _) => await _service.DeleteDistribution(distribution);
+        messageBox.ButtonRightClick += (_, _) => messageBox.Close();
+
+        messageBox.Show("Delete", $"Are you sure you want to delete {distribution.Name}?");
     }
 
     private async Task OnRefreshDistributions()
--- WslToolbox.Gui2/Views/Pages/Dashboard.xaml.cs
+++ WslToolbox.Gui2/Views/Pages/Dashboard.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
-using Wpf.Ui.Common;
 using Wpf.Ui.Common.Interfaces;
-using Wpf.Ui.Controls;
 using Wpf.Ui.Controls.Interfaces;
-using Wpf.Ui.Mvvm.Contracts;
 using WslToolbox.Gui2.Forms;
 using WslToolbox.Gui2.Models;
 using WslToolbox.Gui2.ViewModels;
@@ -13,75 +10,12 @@ namespace WslToolbox.Gui2.Views.Pages;
 
 public partial class Dashboard : INavigableView<DashboardViewModel>
 {
-    private readonly IDialogControl _dialogControl;
-    private readonly ISnackbarControl _snackbarService;
-
-    public Dashboard(DashboardViewModel viewModel, IDialogService dialogService, ISnackbarService snackbarService)
+    public Dashboard(DashboardViewModel viewModel)
     {
-        _dialogControl = dialogService.GetDialogControl();
-        _snackbarService = snackbarService.GetSnackbarControl();
         ViewModel = viewModel;
-
-        ShowEditDistribution = new AsyncRelayCommand<DistributionModel>(OnEditShowDistribution);
-        ShowExportDistribution = new AsyncRelayCommand<DistributionModel>(OnExportDistribution);
-        ShowDeleteDistribution = new RelayCommand<DistributionModel>(OnDeleteDistribution);
 
         InitializeComponent();
     }
-
-    public AsyncRelayCommand<DistributionModel> ShowEditDistribution { get; }
-    public AsyncRelayCommand<DistributionModel> ShowExportDistribution { get; }
-    public RelayCommand<DistributionModel> ShowDeleteDistribution { get; }
-
+    
     public DashboardViewModel ViewModel { get; }
-
-    private async Task OnExportDistribution(DistributionModel? model) => await ViewModel.ExportDistribution.ExecuteAsync(model);
-
-    private void OnDeleteDistribution(DistributionModel? model)
-    {
-        var messageBox = new MessageBox
-        {
-            ButtonLeftName = "Delete",
-            ButtonRightName = "Cancel",
-            ButtonLeftAppearance = ControlAppearance.Danger,
-        };
-
-        messageBox.ButtonLeftClick += async (_, _) => await ViewModel.DeleteDistribution.ExecuteAsync(model);
-        messageBox.ButtonRightClick += (_, _) => messageBox.Close();
-
-        messageBox.Show("Delete", $"Are you sure you want to delete {model.Name}?");
-    }
-
-    private async Task OnEditShowDistribution(DistributionModel? arg)
-    {
-        if (arg == null)
-        {
-            return;
-        }
-
-        var model = new UpdateModel<DistributionModel>
-        {
-            CurrentModel = arg,
-            NewModel = arg.Clone()
-        };
-
-        _dialogControl.Content = new EditDistributionForm {Distribution = model.NewModel};
-
-        _dialogControl.ButtonLeftName = "Save";
-        _dialogControl.ButtonRightName = "Cancel";
-        var dialogResult = await _dialogControl.ShowAndWaitAsync($"Rename - {model.NewModel.Name}", "Enter a new name for your distribution");
-
-        switch (dialogResult)
-        {
-            case IDialogControl.ButtonPressed.Left:
-                ViewModel.EditDistribution.Execute(model);
-                _dialogControl.Hide();
-                break;
-            case IDialogControl.ButtonPressed.Right:
-            case IDialogControl.ButtonPressed.None:
-            default:
-                _dialogControl.Hide();
-                break;
-        }
-    }
 }
