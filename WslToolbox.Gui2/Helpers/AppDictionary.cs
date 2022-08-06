01180ebe358592a414531b7f8206733a0495e013
Peter van der Veen <p.vanderveen93@gmail.com>
2022-08-06 09:09:37 +0200

    Split dictionaries

---
+++ WslToolbox.Gui2/Helpers/AppDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WslToolbox.Gui2.Helpers;
+
+public class AppDictionary : ResourceDictionary
+{
+    private readonly List<string> _dictionaries = new()
+    {
+        "DefaultDictionary.xaml",
+        "SettingsDictionary.xaml",
+    };
+    
+    public AppDictionary()
+    {
+        foreach (var dictionary in _dictionaries)
+        {
+            try
+            {
+                MergedDictionaries.Add(new ResourceDictionary
+                {
+                    Source = new Uri($"pack://application:,,,/WslToolbox.Gui2;component/Resources/Dictionaries/{dictionary}", UriKind.Absolute)
+                });
+            }
+            catch (Exception e)
+            {
+                Debug.Write($"Could not add dictionary {dictionary}: {e.Message}");
+            }
+        }
+    }
+}
