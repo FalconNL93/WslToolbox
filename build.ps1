b9c2e6d6821467c8d4ea141f9f6c3c457c404fef
Peter van der Veen <p.vanderveen93@outlook.com>
2022-11-06 10:36:10 +0100

    Purge older runs

--- build.ps1
+++ build.ps1
@@ -9,11 +9,11 @@ $env:ProductExecutable = 'toolbox.exe';
 $env:ProductName = 'WSL Toolbox';
 $env:ProductDescription = 'WSL Toolbox allows you to manage your WSL Distributions through an easy to use interface.';
 $env:ProductUuid = 'FalconNL93.WSLToolbox';
-$env:ProductVersion = '0.6';
+$env:ProductVersion = '0.6.0';
 $env:ProductEnvironment = 'dev';
 $env:ProductUrl = 'https://github.com/FalconNL93/wsltoolbox';
 $env:SetupOutputFile = 'wsltoolbox-0.6';
 
-#dotnet msbuild $env:Solution_Name /t:Restore /p:Configuration=$env:Configuration
-#dotnet msbuild WslToolbox.UI\WslToolbox.UI.csproj /p:Configuration=$env:Configuration /p:Platform=x64 /p:outdir="$env:GITHUB_WORKSPACE\app\release\x64"
+dotnet msbuild $env:Solution_Name /t:Restore /p:Configuration=$env:Configuration
+dotnet msbuild WslToolbox.UI\WslToolbox.UI.csproj /p:Configuration=$env:Configuration /p:Platform=x64 /p:outdir="$env:GITHUB_WORKSPACE\app\release\x64" /p:Version="$env:ProductVersion"
 & .\WslToolbox.Setup\build.ps1 -BinariesDirectory $env:GITHUB_WORKSPACE\app\release\x64
