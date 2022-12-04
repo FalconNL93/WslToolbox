f1d333a33dbed870f51109f1c1c015f48c350740
Peter van der Veen <p.vanderveen93@gmail.com>
2022-12-04 16:50:11 +0100

    Cleanup

--- WslToolbox.UI/ViewModels/DashboardViewModel.cs
+++ WslToolbox.UI/ViewModels/DashboardViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml.Controls;
 using WslToolbox.Core.Commands.Distribution;
 using WslToolbox.UI.Core.Commands;
 using WslToolbox.UI.Core.Models;
 using WslToolbox.UI.Core.Services;
+using WslToolbox.UI.Extensions;
+using WslToolbox.UI.Views.Modals;
 
 namespace WslToolbox.UI.ViewModels;
 
@@ -15,6 +20,9 @@ public partial class DashboardViewModel : ObservableRecipient
 
     [ObservableProperty]
     private bool _isRefreshing = true;
+    
+    public bool CanRenameDistribution => false;
+    public bool CanMoveDistribution => false;
 
     public DashboardViewModel(DistributionService distributionService,
         ILogger<DashboardViewModel> logger)
@@ -39,5 +47,115 @@ public partial class DashboardViewModel : ObservableRecipient
     private void OnReloadExecution(object? sender, EventArgs e)
     {
         RefreshDistributionsCommand.Execute(null);
+    }
+    
+    [RelayCommand]
+    private async Task StartAllDistribution()
+    {
+        foreach (var distribution in Distributions)
+        {
+            await _distributionService.StartDistribution(distribution);
+        }
+    }
+
+    [RelayCommand]
+    private async Task StopAllDistribution()
+    {
+        foreach (var distribution in Distributions)
+        {
+            await _distributionService.StopDistribution(distribution);
+        }
+    }
+
+    [RelayCommand]
+    private async Task RestartAllDistribution()
+    {
+        foreach (var distribution in Distributions)
+        {
+            await _distributionService.RestartDistribution(distribution);
+        }
+    }
+
+    [RelayCommand]
+    private async Task AddDistribution(Page page)
+    {
+        var available = await _distributionService.ListInstallableDistributions();
+        var distributions = available.Where(x => !x.IsInstalled).ToList();
+        try
+        {
+            var installDistribution = await page.ShowModal<AddDistribution>(
+                distributions, "Add distribution", "Add");
+
+            if (installDistribution.Modal == null || installDistribution.ContentDialogResult != ContentDialogResult.Primary)
+            {
+                return;
+            }
+
+            _distributionService.InstallDistribution(installDistribution.Modal.GetSelectedItem<Distribution>());
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Could not start {Message}", e.Message);
+        }
+    }
+
+    [RelayCommand]
+    private async Task RefreshDistributions()
+    {
+        _logger.LogInformation("Refreshing list of distributions");
+        try
+        {
+            IsRefreshing = true;
+            Distributions.Clear();
+            (await _distributionService.ListDistributions()).ToList()
+                .ForEach(distribution =>
+                {
+                    Distributions.Add(distribution);
+                });
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            IsRefreshing = false;
+        }
+    }
+
+    [RelayCommand]
+    private async Task DeleteDistribution(Distribution? distribution)
+    {
+        await _distributionService.DeleteDistribution(distribution);
+    }
+
+    [RelayCommand]
+    private async Task RestartDistribution(Distribution? distribution)
+    {
+        await _distributionService.StopDistribution(distribution);
+        await _distributionService.StartDistribution(distribution);
+    }
+
+    [RelayCommand]
+    private async Task StopDistribution(Distribution? distribution)
+    {
+        await _distributionService.StopDistribution(distribution);
+    }
+
+    [RelayCommand]
+    private async Task StartDistribution(Distribution? distribution)
+    {
+        await _distributionService.StartDistribution(distribution);
+    }
+
+    [RelayCommand(CanExecute = nameof(CanRenameDistribution))]
+    private async Task RenameDistribution(Distribution? distribution)
+    {
+    }
+
+    [RelayCommand(CanExecute = nameof(CanMoveDistribution))]
+    private async Task MoveDistribution(Distribution? distribution)
+    {
     }
 }
--- WslToolbox.UI/ViewModels/SettingsViewModel.cs
+++ WslToolbox.UI/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.Options;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using WslToolbox.UI.Contracts.Services;
+using WslToolbox.UI.Core.Helpers;
 using WslToolbox.UI.Core.Models;
 using WslToolbox.UI.Core.Services;
+using WslToolbox.UI.Helpers;
+using WslToolbox.UI.Notifications;
 using WslToolbox.UI.Services;
 
 namespace WslToolbox.UI.ViewModels;
@@ -26,6 +31,9 @@ public partial class SettingsViewModel : ObservableRecipient
 
     [ObservableProperty]
     private bool _isPackage;
+    
+    [ObservableProperty]
+    private ElementTheme _elementTheme;
 
     public SettingsViewModel(IThemeSelectorService themeSelectorService,
         IOptions<UserOptions> userOptions,
@@ -53,4 +61,107 @@ public partial class SettingsViewModel : ObservableRecipient
     public RunOptions RunOptions { get; }
 
     public ObservableCollection<string> Themes { get; set; } = new(Enum.GetNames(typeof(ElementTheme)));
+    
+    [RelayCommand]
+    private async Task CheckForUpdates()
+    {
+        UpdaterResult = new UpdateResultModel {UpdateStatus = "Checking for updates..."};
+        UpdaterResult = await _updateService.GetUpdateDetails();
+
+        if (!UpdaterResult.UpdateAvailable)
+        {
+            _appNotificationService.ShowNoUpdatesNotification();
+        }
+        else
+        {
+            _appNotificationService.ShowUpdatesAvailableNotification(UpdaterResult);
+            var result = await _messenger.ShowUpdateDialog(new UpdateViewModel
+            {
+                EnableInstallUpdate = true,
+                CurrentVersion = UpdaterResult.CurrentVersion,
+                LatestVersion = UpdaterResult.LatestVersion
+            });
+
+            if (result == ContentDialogResult.Primary)
+            {
+                ShellHelper.OpenUrl(UpdaterResult.DownloadUri);
+            }
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(10));
+    }
+
+    [RelayCommand]
+    private async Task ThemeChange(ElementTheme param)
+    {
+        if (ElementTheme == param)
+        {
+            return;
+        }
+
+        ElementTheme = param;
+        await _themeSelectorService.SetThemeAsync(param);
+    }
+
+    [RelayCommand(CanExecute = nameof(CanOpenLogFile))]
+    private static void OpenLogFile()
+    {
+        try
+        {
+            ShellHelper.OpenFile(Toolbox.LogFile);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private static bool CanOpenLogFile()
+    {
+        return File.Exists(Toolbox.LogFile);
+    }
+
+    [RelayCommand]
+    private static void OpenAppDir()
+    {
+        try
+        {
+            ShellHelper.OpenFile(Toolbox.AppDirectory);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    [RelayCommand]
+    private void SaveConfiguration()
+    {
+        _configurationService.Save(UserOptions);
+    }
+
+    [RelayCommand]
+    private void RestoreDefaultConfiguration()
+    {
+        _configurationService.Restore<UserOptions>();
+
+        RestoreDefaultConfigurationCommand.NotifyCanExecuteChanged();
+        OpenConfigurationCommand.NotifyCanExecuteChanged();
+    }
+
+    [RelayCommand]
+    private static void OpenConfiguration()
+    {
+        try
+        {
+            ShellHelper.OpenFile(Toolbox.UserConfiguration);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
